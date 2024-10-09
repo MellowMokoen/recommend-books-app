3b81f4ac568f1be4000e302034392c70d514ee4b
Mamello <mamello3632@gmail.com>
2024-10-09 20:45:12 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+    
+  colors: {
+    nude: '#d8b4a0', 
+    white: '#ffffff', 
+    black: '#000000', 
+    brown: '#8b4513',
+  },
+
+
+      fontFamily: {
+        sans: ['Jost', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
