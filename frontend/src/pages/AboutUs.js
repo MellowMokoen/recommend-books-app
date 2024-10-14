3af8b0b0b73cd562b939babbe43f912a11fe389b
Mamello <mamello3632@gmail.com>
2024-10-14 02:34:14 +0200

    Added a contact/follow us section

--- frontend/src/pages/AboutUs.js
+++ frontend/src/pages/AboutUs.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import background3 from '../images/background3.jpg';
+import { FaInstagram, FaTiktok, FaLinkedin } from 'react-icons/fa';
 
 const AboutUs = () => {
   return (
     <div className="h-screen flex items-center justify-center relative">
-      {/* Background Image */}
+    
       <div
         className="absolute inset-0"
         style={{
@@ -14,7 +15,7 @@ const AboutUs = () => {
         }}
       ></div>
       
-      {/* Content Container */}
+      
       <div className="bg-black text-nude p-12 rounded-lg shadow-lg z-10">
         <h1 className="text-4xl mb-4 font-bold text-center">- About Us</h1>
         <p className="text-lg mb-12">
@@ -25,7 +26,23 @@ const AboutUs = () => {
         <p className="text-lg">
           Our vision is to create a vibrant community of readers who can share their love for literature. We aspire to be the go-to platform for discovering not only new releases but also hidden gems in every genre. By continuously enhancing our services, we aim to empower readers and promote a lifelong love of reading.
         </p>
+        <div className="follow-us mt-12">
+          <h3 className="text-md mb-2">Follow Us</h3>
+          <div className="flex space-x-4">
+            <a href="https://www.instagram.com/mellow_mokoena" target="_blank" rel="noopener noreferrer" className="text-nude hover:text-white">
+              <FaInstagram size={20} />
+            </a>
+            <a href="https://www.tiktok.com/@mellow_mokoena" target="_blank" rel="noopener noreferrer" className="text-nude hover:text-white">
+              <FaTiktok size={20} />
+            </a>
+            <a href="https://www.linkedin.com/in/mamello-mokoena-170744204/" target="_blank" rel="noopener noreferrer" className="text-nude hover:text-white">
+              <FaLinkedin size={20} />
+            </a>
+          </div>
+        </div>
       </div>
+
+      
     </div>
   );
 };
