5d4193062416fe9cbc6d8a70538b08dafd8c6370
Mamello <mamello3632@gmail.com>
2024-10-14 19:40:56 +0200

    Replaced hardcoded value with environment variables

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
-// Create a connection to the database
+// Create a connection to the MySQL database using environment variables
 const connection = mysql.createConnection({
-  host: 'localhost',    // Your MySQL host (usually localhost)
-  user: 'root',         // Your MySQL root user (or any user you've set up)
-  password: 'Paballo1327@',         // Your MySQL password (replace with your password)
-  database: 'books_db' // Your database name
+  host: process.env.DB_HOST,      // Use the DB_HOST environment variable
+  user: process.env.DB_USER,      // Use the DB_USER environment variable
+  password: process.env.DB_PASS,   // Use the DB_PASS environment variable
+  database: process.env.DB_NAME    // Use the DB_NAME environment variable
 });
 
 // Connect to the MySQL database
